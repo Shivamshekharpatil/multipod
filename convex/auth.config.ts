6e53c9b43ca8c884c7851ab4281b4376d921336b
Shivam Shekhar Patil <patilshivam2617@gmail.com>
2024-07-06 20:35:06 +0100

    Integrated clerk wth project which will help in authenticating the user.

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+      {
+        domain: "https://rapid-koala-1.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig;
