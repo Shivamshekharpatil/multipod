dde09b380fced08f6d907c50fe29d0bf0ef5cc13
Shivam Shekhar Patil <patilshivam2617@gmail.com>
2024-07-09 03:13:38 +0100

    Created useStates for future use.

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -29,6 +29,7 @@ import { useState } from "react"
 import { Textarea } from "@/components/ui/textarea"
 import GeneratePodcast from "@/components/GeneratePodcast"
 import GenerateThumbnail from "@/components/GenerateThumbnail"
+import { Id } from "@/convex/_generated/dataModel"
 
 
 const voiceCategories = ['alloy', 'shimmer', 'nova', 'echo', 'fable', 'onyx'];
@@ -40,7 +41,17 @@ const formSchema = z.object({
 })
 
 const CreatePodcast = () => {
+  const [imagePrompt, setImagePrompt] = useState('');
+  const [imageStorageId, setImageStorageId] = useState<Id<"_storage"> | null>(null)
+  const [imageUrl, setImageUrl] = useState('');
+  
+  const [audioUrl, setAudioUrl] = useState('');
+  const [audioStorageId, setAudioStorageId] = useState<Id<"_storage"> | null>(null)
+  const [audioDuration, setAudioDuration] = useState(0);
+
+  const [voicePrompt, setVoicePrompt] = useState('');
   const [voiceType, setVoiceType] = useState<string | null>(null);
+
   const [isSubmitting, setIsSubmitting] = useState(false);
 
   // 1. Define your form.
