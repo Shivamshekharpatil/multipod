dbca822437754a6825212a260b0d9a28a89c14f9
Shivam Shekhar Patil <patilshivam2617@gmail.com>
2024-07-09 02:20:58 +0100

    Started working on create podcast page.

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -1,10 +1,102 @@
-import React from 'react'
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Section } from "lucide-react"
+import { Label } from "@/components/ui/label"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { cn } from "@/lib/utils"
+
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+})
 
 const CreatePodcast = () => {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
   return (
-    <div>
-      <h1 className='text-20 font-bold text-white-1'>Create Podcast</h1>
-    </div>
+    <section className="mt-10 flex flex-col">
+      <h1 className="text-20 font-bold text-white-1">Create Podcast</h1>
+
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="mt-12 flex w-full flex-col">
+        <div className="flex flex-col gap-[30px] border-b border-black-5 pb-10">
+        <FormField
+          control={form.control}
+          name="podcastTitle"
+          render={({ field }) => (
+            <FormItem className="flex flex-col gap-2.5">
+              <FormLabel className="text-16 font-bold text-white-1">Username</FormLabel>
+              <FormControl>
+                <Input className="input-class focus-visible:ring-blue-1" 
+                placeholder="MultipodAI Podcast" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage className="text-white-1" />
+            </FormItem>
+          )}
+        />
+
+        <div className="flex flex-col gap-2.5">
+          <Label className="text-16 font-bold text-white-1">
+            Choose AI Voice.
+          </Label>
+
+          <Select>
+          <SelectTrigger className={cn
+            ('text-16 w-full border-none bg-black-1 text-gray-1')}>
+            <SelectValue placeholder="Choose AI Voice" 
+            className="placeholder:text-gray-1" />
+            </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="light">Light</SelectItem>
+            <SelectItem value="dark">Dark</SelectItem>
+            <SelectItem value="system">System</SelectItem>
+            </SelectContent>
+          </Select>
+
+        </div>
+        </div>
+      </form>
+    </Form>
+    </section>
   )
 }
 
