6fe44305266c4a9c5d52e391765566071689c9d4
Shivam Shekhar Patil <patilshivam2617@gmail.com>
2024-07-12 13:20:54 +0100

    Destructured GeneratePodcast.

--- components/GeneratePodcast.tsx
+++ components/GeneratePodcast.tsx
@@ -1,9 +1,31 @@
+import { GeneratePodcastProps } from '@/types'
 import React from 'react'
+import { Label } from './ui/label'
+import { Textarea } from './ui/textarea'
 
-const GeneratePodcast = () => {
+const GeneratePodcast = ({
+    setAudioStorageId,
+    setAudio,
+    voiceType,
+    audio,
+    voicePrompt,
+    setVoicePrompt,
+    setAudioDuration,
+}: GeneratePodcastProps) => {
   return (
     <div>
-      GeneratePodcast
+        <div className="flex flex-col gap-2.5">
+            <Label className="text-16 font-bold text-white-1">
+                Write an AI prompt to Generate Podcast.
+            </Label>
+            <Textarea
+            className="input-class font-light focus-visible:ring-offset-blue-500"
+            placeholder='Provide Text to generate audio'
+            rows={5}
+            value={voicePrompt}
+            onChange={(e) => setVoicePrompt(e.target.value)}
+            />
+        </div>
     </div>
   )
 }
