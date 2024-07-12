509054d787c98ab55a4142bced0971eb3218df69
Shivam Shekhar Patil <patilshivam2617@gmail.com>
2024-07-12 23:55:13 +0100

    Created an action in service and added open ai to convex.

--- components/GeneratePodcast.tsx
+++ components/GeneratePodcast.tsx
@@ -4,11 +4,15 @@ import { Label } from './ui/label'
 import { Textarea } from './ui/textarea'
 import { Button } from './ui/button'
 import { Loader } from 'lucide-react'
+import { useAction } from 'convex/react'
+import { api } from '@/convex/_generated/api'
 
 const useGeneratePodcast = ({
     setAudio, voiceType, voicePrompt, setAudioStorageId
 }: GeneratePodcastProps) => {
     const [isGenerating, setIsGenerating] = useState(false);
+
+    const getPodcastAudio = useAction(api.openai.generateAudioAction)
 
     const generatePodcast = async () => {
         setIsGenerating(true);
@@ -20,10 +24,10 @@ const useGeneratePodcast = ({
         }
 
         try {
-            /* const response = await getPodcastAudio({
+            const response = await getPodcastAudio({
                 voice: voiceType,
                 input: voicePrompt
-            }) */
+            })
         } catch (error) {
             console.log('Error generating podcast', error)
             //todo: show error message
