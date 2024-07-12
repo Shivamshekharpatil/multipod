b48d6eacb683dee492ab361b5aef671f4e022e01
Shivam Shekhar Patil <patilshivam2617@gmail.com>
2024-07-12 14:15:08 +0100

    added if & else and ty & catch method to call audio generate.

--- components/GeneratePodcast.tsx
+++ components/GeneratePodcast.tsx
@@ -13,6 +13,22 @@ const useGeneratePodcast = ({
     const generatePodcast = async () => {
         setIsGenerating(true);
         setAudio('');
+
+        if(!voicePrompt) {
+            //todo: show error message
+            return setIsGenerating(false);
+        }
+
+        try {
+            /* const response = await getPodcastAudio({
+                voice: voiceType,
+                input: voicePrompt
+            }) */
+        } catch (error) {
+            console.log('Error generating podcast', error)
+            //todo: show error message
+            setIsGenerating(false);
+        }
     }
 
     return { isGenerating, generatePodcast}
